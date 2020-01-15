1acfd0a1d9168b5edcdc5ef38c4fdfc275957f23
Slava <slava.leb.oren@gmail.com>
2020-01-15 11:40:05 +0300

    Создание крипов

---
+++ creep.create.js
@@ -0,0 +1,3 @@
+function creepCreate() {}
+
+module.exports = creepCreate;
--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-module.exports.loop = function() {};
+module.exports.loop = function() {
+    // Перебор всех моих комнат через room мы будем обращаться ко всем структурам
+    for (var name in Game.rooms) {
+        var room = Game.rooms[name];
+
+        // Создание крипов
+    }
+};
