0eccd0039d99de97a98370102eaf737735df5295
Slava <slava.leb.oren@gmail.com>
2020-01-15 12:30:07 +0300

    Функция переделывает объект с частями крипа в массив пригодный для указания в качестве параметра для функции создания

--- creep.create.js
+++ creep.create.js
@@ -1,3 +1,24 @@
-function creepCreate() {}
+// Создание крипов
 
-module.exports = creepCreate;
+module.exports = function(stage, room) {
+    switch (stage) {
+        case 1:
+            creepCreate({ MOVE: 2, WORK: 1, CARRY: 1 });
+            break;
+    }
+};
+
+function creepCreate(composition) {
+    console.log(creepConstraction(composition));
+}
+
+// Функция переделывает объект с частями крипа в массив пригодный для указания в качестве параметра для функции создания
+function creepConstraction(composition) {
+    var constr = [];
+    for (var name in composition) {
+        for (let i = 0; i < composition[name]; i++) {
+            constr.push(name);
+        }
+    }
+    return constr;
+}
