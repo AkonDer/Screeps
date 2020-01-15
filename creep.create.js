0bfb45721548a2227faf48fd3d3229a0afd336f8
Slava <slava.leb.oren@gmail.com>
2020-01-15 16:44:29 +0300

    Реализовал функцию создания крипов по шаблону

--- creep.create.js
+++ creep.create.js
@@ -4,19 +4,39 @@ var helper = require("helper");
 module.exports = function(stage, room) {
     switch (stage) {
         case 1:
-            creepCreate({ builder: 1, miner: 1, carrier: 1 }, room);
+            creepCreate(
+                {
+                    builder: [1, { CARRY: 1, MOVE: 1 }],
+                    carrier: [1, { CARRY: 1, MOVE: 1 }],
+                    miner: [2, { CARRY: 1, MOVE: 1 }]
+                },
+                room
+            );
             break;
     }
 };
 
+// Функция создания крипа
 function creepCreate(creeps, room) {
-    switch (room.energyAvailable) {
-        case 300:
-            console.log(helper.getFreeSpawn(room));
-            break;
+    for (let name in room.creeps) {
+        console.log(room.creeps[name].name);
     }
 
-    // { MOVE: 2, WORK: 2, CARRY: 1 },
+    for (let name in creeps) {
+        let creepTypeQuantity = _.filter(Game.creeps, creep => creep.memory.role == name); //TODO Заменить на фильтрацию крипов в конкретной комнате
+        if (creepTypeQuantity.length < creeps[name][0]) {
+            for (let name in Memory.creeps) {
+                if (!Game.creeps[name]) {
+                    delete Memory.creeps[name];
+                    console.log("Clearing non-existing creep memory:", name);
+                }
+            }
+            let spawn = helper.getFreeSpawn(room);
+            let crecon = creepConstraction(creeps[name][1]);
+            let newName = name + Game.time;
+            spawn.spawnCreep(crecon, newName, { memory: { role: name } });
+        }
+    }
 }
 
 // Функция переделывает объект с частями крипа в массив пригодный для указания в качестве параметра для функции создания
@@ -24,7 +44,7 @@ function creepConstraction(composition) {
     let constr = [];
     for (let name in composition) {
         for (let i = 0; i < composition[name]; i++) {
-            constr.push(name);
+            constr.push(name.toLowerCase());
         }
     }
     return constr;
--- main.js
+++ main.js
@@ -4,7 +4,6 @@ module.exports.loop = function() {
     // Перебор всех моих комнат через room мы будем обращаться ко всем структурам
     for (var name in Game.rooms) {
         var room = Game.rooms[name];
-
         // Разделение на этапы в зависимости от уровня контроллера
         switch (room.controller.level) {
             case 1:
