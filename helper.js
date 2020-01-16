eada33313da1d2599f768f653c9feb644e6266c7
Slava <slava.leb.oren@gmail.com>
2020-01-16 21:06:58 +0300

    Добавил в хелпер функцию добавления в память  источников энергии

--- helper.js
+++ helper.js
@@ -27,6 +27,16 @@ module.exports = {
     // Вибирает таргет до которого наименьшее растояние
     getMinRange: function(creep, targets) {
         return getMinRange(creep, targets);
+    },
+
+    // Добавить все источники энергии в память комнаты
+    addSourceToMemory: function(room) {
+        let sources = room.find(FIND_SOURCES);
+        const pos = room.getPositionAt(room.memory.centerBase.x, room.memory.y);
+        let ranges = sources.map(source => {
+            return [source.pos.getRangeTo(pos), source.id];
+        });
+        room.memory.sources = ranges.sort();
     }
 };
 
