adc8f00b2676cdac1c784e6f8e70f7f4264a240c
Slava <slava.leb.oren@gmail.com>
2020-01-17 12:49:22 +0300

    Реализованна функция нахождения количества расширений в комнате

--- stage2.js
+++ stage2.js
@@ -4,15 +4,17 @@ let stage2_2 = require("stage2_2");
 module.exports = function(room) {
     stage2_1(room);
 
-    let extQuantity = room.find(FIND_STRUCTURES, {
+    // Если расширений 5 то произвести следующие действия
+    if (findExtentionQuantity(room) == 5) {
+        stage2_2(room);
+    }
+};
+
+// Нахождение количества расширений в комнате
+function findExtentionQuantity(room) {
+    return room.find(FIND_STRUCTURES, {
         filter: structure => {
             return structure.structureType == STRUCTURE_EXTENSION;
         }
     }).length;
-
-    // Если расширений 5 то произвести следующие действия
-    if (extQuantity == 5) {
-        stage2_2(room);
-    }
-};
-function findExtentionQuantity() {}
+}
